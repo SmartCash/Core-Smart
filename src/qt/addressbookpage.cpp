b84127c3f2e2c1a171c6fb1142b738556fd8f4a5
thesolarminer <solarminer@smartcash.cc>
2017-11-06 16:40:22 -0600

    Change button and gui to disabled

--- src/qt/addressbookpage.cpp
+++ src/qt/addressbookpage.cpp
@@ -76,7 +76,8 @@ AddressBookPage::AddressBookPage(Mode mode, Tabs tab, QWidget *parent) :
         ui->zerocoinSpendButton->setVisible(false);
         break;
     case ZerocoinTab:
-        ui->labelExplanation->setText(tr("These are your private coins from the Renew SmartCash operation.  Select an amount to Renew and wait about 10 minutes before you select Reclaim.  You can increase anonymity waiting longer to reclaim and using more of the same amount."));
+//        ui->labelExplanation->setText(tr("These are your private coins from the Renew SmartCash operation.  Select an amount to Renew and wait about 10 minutes before you select Reclaim.  You can increase anonymity waiting longer to reclaim and using more of the same amount."));
+        ui->labelExplanation->setText(tr("Renew and Reclaim Features are temporarily disabled")
         ui->deleteAddress->setVisible(false);
         ui->signMessage->setVisible(false);
         ui->newAddress->setVisible(false);
