df4e704f625f79513c8aaa677743a4475dd6277b
thesolarminer <thesolarminer@users.noreply.github.com>
2018-02-02 13:24:49 -0600

    Logging Update

--- src/smartnode/smartnode.cpp
+++ src/smartnode/smartnode.cpp
@@ -148,7 +148,7 @@ arith_uint256 CSmartnode::CalculateScore(const uint256& blockHash) {
     CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
     ss << vin.prevout << nCollateralMinConfBlockHash << blockHash;
     
-    LogPrint("Hashtest","SmartNode Score\n", ss.GetHash().ToString());
+    LogPrint("Hashtest","SmartNode Score %s\n", ss.GetHash().ToString());
 
     return UintToArith256(ss.GetHash());
 
