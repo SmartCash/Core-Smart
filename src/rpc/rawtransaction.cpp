f1f429a9f5630964447eba1f7e84605a41135b71
dustinface <xdustinfacex@gmail.com>
2018-12-17 15:11:08 +0100

    rpc: Missing " after escape sequence

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -628,7 +628,7 @@ UniValue decoderawtransaction(const UniValue& params, bool fHelp)
             "         \"reqSigs\" : n,            (numeric) The required sigs\n"
             "         \"type\" : \"pubkeyhash\",  (string) The type, eg 'pubkeyhash'\n"
             "         \"addresses\" : [           (json array of string)\n"
-            "           \S2tvKAXCxZjSmdNbao16dKXC8tRWfcF5oc\"   (string) smartcash address\n"
+            "           \"S2tvKAXCxZjSmdNbao16dKXC8tRWfcF5oc\"   (string) smartcash address\n"
             "           ,...\n"
             "         ]\n"
             "       }\n"
