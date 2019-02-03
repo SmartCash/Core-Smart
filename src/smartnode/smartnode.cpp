500e59dd7bf62d4635d0e1091d5d3eda15a8dcd9
thesolarminer <thesolarminer@users.noreply.github.com>
2019-02-02 19:27:10 -0600

    Bug Fix

--- src/smartnode/smartnode.cpp
+++ src/smartnode/smartnode.cpp
@@ -128,7 +128,7 @@ CSmartnode::CollateralStatus CSmartnode::CheckCollateral(const COutPoint& outpoi
           return COLLATERAL_INVALID_AMOUNT;
       }
     }else{
-      if(nHeight >= (TESTNET_V1_2_8_SMARNODE_NEW_COLLATERAL_HEIGHT - 10000){
+      if(nHeight >= (TESTNET_V1_2_8_SMARNODE_NEW_COLLATERAL_HEIGHT - 10000)){
         if(coin.out.nValue != SMARTNODE_COIN_REQUIRED_V2 * COIN){
           return COLLATERAL_INVALID_AMOUNT;
         }
