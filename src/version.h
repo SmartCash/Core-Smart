483524dec49a72bcbfaa33c72230aa51b4bed0b1
thesolarminer <thesolarminer@users.noreply.github.com>
2019-02-06 03:46:27 -0600

    Testnet Change

--- src/version.h
+++ src/version.h
@@ -9,13 +9,13 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 90027;
+static const int PROTOCOL_VERSION = 90028;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 90013;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 90027;
+static const int GETHEADERS_VERSION = 90028;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 90027;
