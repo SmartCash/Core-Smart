215bfadb6708e718561f8e6f9cffaf4b07a4d4a1
thesolarminer <thesolarminer@users.noreply.github.com>
2019-02-06 15:18:57 -0600

    Testnet updates

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@ static const int INIT_PROTO_VERSION = 90013;
 static const int GETHEADERS_VERSION = 90028;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 90027;
+static const int MIN_PEER_PROTO_VERSION = 90028;
 
 //! first version with multi node payments
 static const int MIN_MULTIPAYMENT_PROTO_VERSION = 90026;
