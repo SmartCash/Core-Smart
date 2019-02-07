80aa804f721ba42b934450a25ad7bf69360ea4cf
thesolarminer <thesolarminer@users.noreply.github.com>
2019-02-06 19:51:55 -0600

    Changing back versions

--- src/version.h
+++ src/version.h
@@ -9,16 +9,16 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 90028;
+static const int PROTOCOL_VERSION = 90027;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 90013;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 90028;
+static const int GETHEADERS_VERSION = 90026;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 90028;
+static const int MIN_PEER_PROTO_VERSION = 90026;
 
 //! first version with multi node payments
 static const int MIN_MULTIPAYMENT_PROTO_VERSION = 90026;
