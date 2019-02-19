8964f1d7375515a70a5a51b41e461da1f3a87af5
dustinface <xdustinfacex@gmail.com>
2019-02-20 00:08:31 +0100

    Increased protocol version to 90028

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
 static const int PROTOCOL_BASE_VERSION = 90000;
 static const int PROTOCOL_MAX_VERSION = 90000 + 0xFF;
 
-static const int PROTOCOL_VERSION = 90027;
+static const int PROTOCOL_VERSION = 90028;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 90013;
@@ -27,7 +27,7 @@ static const int MIN_PEER_PROTO_VERSION = 90026;
 static const int MIN_MULTIPAYMENT_PROTO_VERSION = 90026;
 
 //! first version with higher node collaterals
-static const int MIN_HIGH_COLLATERAL_PROTO_VERSION = 90027;
+static const int MIN_HIGH_COLLATERAL_PROTO_VERSION = 90028;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
