b8f528d4044eadd10797d22158d1d7011cc9adf5
dustinface <xdustinfacex@gmail.com>
2019-02-20 00:47:20 +0100

    rpc: Added command to generate a random pk/address keypair

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -139,6 +139,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "getaddressdeltas", 0},
     { "getaddressutxos", 0},
     { "getaddressmempool", 0},
+    { "getrandomkeypair", 0},
 };
 
 class CRPCConvertTable
