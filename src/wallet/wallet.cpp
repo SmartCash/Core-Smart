5fd9ac4e0417c0dd8d26c4f7158193d039bc348e
dustinface <xdustinfacex@gmail.com>
2019-10-17 16:11:17 +0200

    wallet: Fixed inputs nSequence value to activate nLockTime

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3254,7 +3254,7 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
                 // Note how the sequence number is set to max()-1 so that the
                 // nLockTime set above actually works.
                 BOOST_FOREACH(const PAIRTYPE(const CWalletTx *, unsigned int) &coin, setCoins)
-                    txNew.vin.push_back(CTxIn(coin.first->GetHash(), coin.second, CScript(), std::numeric_limits < unsigned int > ::max()));
+                    txNew.vin.push_back(CTxIn(coin.first->GetHash(), coin.second, CScript(), std::numeric_limits < unsigned int > ::max() - 1));
 
                 // Sign
                 int nIn = 0;
