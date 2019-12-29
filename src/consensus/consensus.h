80df8e1d6abd648250950e88be8a345a13265089
thesolarminer <thesolarminer@users.noreply.github.com>
2019-12-29 02:34:23 -0600

    Update Block Buffer

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -9,7 +9,7 @@
 #include <stdint.h>
 
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
-static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 8000000;
+static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 16000000;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
 static const unsigned int MAX_BLOCK_WEIGHT = 32000000;
 /** The maximum allowed size for a block excluding witness data, in bytes (network rule) */
