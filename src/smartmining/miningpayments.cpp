df9bb2d114e84d4e91bf3d3018ffdcda6c479917
Solarminer <shrewmm@netscape.net>
2019-12-31 00:37:07 -0600

    Miningreward change block

--- src/smartmining/miningpayments.cpp
+++ src/smartmining/miningpayments.cpp
@@ -220,7 +220,12 @@ static bool CheckSignature(const CBlock &block, const CBlockIndex *pindex)
 
 CAmount GetMiningReward(CBlockIndex * pindex, CAmount blockReward)
 {
+    if( pindex->nHeight < HF_V1_3_HEIGHT ){ 
+    return blockReward / 20; // 5%
+    }
+    else {
     return blockReward / 100; // 1%
+    }
 }
 
 void SmartMining::FillPayment(CMutableTransaction& coinbaseTx, int nHeight, CBlockIndex * pindexPrev, CAmount blockReward, CTxOut &outSignature, const CSmartAddress &signingAddress)
